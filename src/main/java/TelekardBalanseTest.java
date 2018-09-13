0d467347107dbaade2ff5c58e1cd40d6e0320d33
dinar <niflid.88@gmail.com>
2018-09-14 00:21:30 +0300

    еще цифры

--- src/main/java/TelekardBalanseTest.java
+++ src/main/java/TelekardBalanseTest.java
@@ -19,7 +19,7 @@ public class TelekardBalanseTest {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        // Set android deviceName desired capability. Set your device name. ттт98787987
+        // Set android deviceName desired capability. Set your device name. ттт9878798787
         capabilities.setCapability("deviceName", "D6503");
 
         // Set BROWSER_NAME desired capability. It's Android in our case here.
