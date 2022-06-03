89f136ea3352ee753374e82db1754396a7326552
dinar <niflid.88@gmail.com>
2022-06-03 22:47:52 +0300

    New

---
+++ src/test/java/Fearless/Example.java
@@ -0,0 +1,15 @@
+package Fearless;
+
+import org.testng.annotations.Test;
+
+public class Example {
+    @Test
+    public void ex(){
+
+        int a = 1;
+        int b =2;
+        System.out.println(a + b);
+
+    }
+
+}
